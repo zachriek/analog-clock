23c5a7f0699838ce2678a0e34b009429dde663a5
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2021-06-18 09:03:46 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const mn = document.querySelector('#mn');
 const sc = document.querySelector('#sc');
 
 setInterval(() => {
-
 	let day = new Date();
 	let hh = day.getHours() * 30;
 	let mm = day.getMinutes() * deg;
@@ -13,12 +12,4 @@ setInterval(() => {
 	hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
 	mn.style.transform = `rotateZ(${mm}deg)`;
 	sc.style.transform = `rotateZ(${ss}deg)`;
-
-});
-
-const menuUL = document.querySelector('nav.menu-container ul');
-const menuToggle = document.querySelector('nav.menu-container .menu-toggle .toggler');
-
-menuToggle.addEventListener('click', function() {
-	menuUL.classList.toggle('slide');
 });
